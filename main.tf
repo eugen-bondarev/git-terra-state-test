21aa93aa61cc6c7d92b050235d357b54f8ee0702
Eugen <eug.bondarev@gmail.com>
2024-01-13 11:03:39 +0100

    setting up a test terraform project

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  project     = "k8s-test-358716"
+}
+
+resource "google_artifact_registry_repository" "git-terra-state-test" {
+  location      = "europe-west3"
+  repository_id = "my-repository"
+  description   = "example docker repository"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}
