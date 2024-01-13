a8789f8fa23dce8ad246624a0723df7dee9bb531
Eugen <eug.bondarev@gmail.com>
2024-01-13 11:19:55 +0100

    destroying the artifact registry

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "google" {
   project     = "k8s-test-358716"
 }
 
-resource "google_artifact_registry_repository" "git-terra-state-test" {
-  location      = "europe-west3"
-  repository_id = "my-repository"
-  description   = "example docker repository"
-  format        = "DOCKER"
+# resource "google_artifact_registry_repository" "git-terra-state-test" {
+#   location      = "europe-west3"
+#   repository_id = "my-repository"
+#   description   = "example docker repository"
+#   format        = "DOCKER"
 
-  docker_config {
-    immutable_tags = true
-  }
-}
+#   docker_config {
+#     immutable_tags = true
+#   }
+# }
