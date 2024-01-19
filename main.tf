80e9ba0cbaeaec299d472d473506430d1aa54cb9
Eugen <eug.bondarev@gmail.com>
2024-01-19 13:52:01 +0100

    removing registry

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "google" {
   project     = "k8s-test-358716"
 }
 
-resource "google_artifact_registry_repository" "git-terra-state-test" {
-  location      = "europe-west3"
-  repository_id = "my-repository"
-  description   = "example docker repository"
-  format        = "DOCKER"
-  docker_config {
-    immutable_tags = true
-  }
-}
+# resource "google_artifact_registry_repository" "git-terra-state-test" {
+#   location      = "europe-west3"
+#   repository_id = "my-repository"
+#   description   = "example docker repository"
+#   format        = "DOCKER"
+#   docker_config {
+#     immutable_tags = true
+#   }
+# }
